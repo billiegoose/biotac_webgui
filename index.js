adfbf62d4f83a63c2ccf7bbd92a40ceeeb4d33b7
Ryan Young <ry525525@gmail.com>
2014-01-28 12:04:48 -0500

    Made it do what we want kinda sort of almost

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ maxVelX.get(function(value) {
 
 // WMH added 2013-01-27
 function rgb(r, g, b){
-  return ["rgb(",r,",",g,",",b,")"].join("");
+  return ["rgb(",r,",",g,",",b,")"].join(""); //Joining array of strings
 }
 
 var id = 1; 
@@ -91,3 +91,36 @@ var val = 10;
 $('#cell'+id).html(val.toString());
 // example of how to set cell color
 $('#cell'+id).css('background-color',rgb(250,150,0));
+
+var listener = new ROSLIB.Topic({
+  ros : ros,
+  name : '/biotac_pub',
+  messageType : 'biotac_sensors/BioTacHand'
+});
+
+listener.subscribe(function(message) {
+  console.log('Received message on ' + listener.name + ': ' + message.bt_data[0].electrode_data);
+  //console.log(message.bt_data[0].electrode_data);
+  var data = message.bt_data[0].electrode_data;
+  $('#cell3').html(data[6]);
+  $('#cell7').html(data[8]);
+  $('#cell9').html(data[7]);
+  $('#cell11').html(data[10]);
+  $('#cell13').html(data[9]);
+  $('#cell15').html(data[0]);
+  $('#cell17').html(data[11]);
+  $('#cell18').html(data[16]);
+  $('#cell19').html(data[1]);
+  $('#cell22').html(data[12]);
+  $('#cell24').html(data[2]);
+  $('#cell26').html(data[13]);
+  $('#cell30').html(data[3]);
+  $('#cell32').html(data[14]);
+  $('#cell33').html(data[17]);
+  $('#cell34').html(data[4]);
+  $('#cell36').html(data[15]);
+  $('#cell38').html(data[18]);
+  $('#cell40').html(data[5]);
+
+  //listener.unsubscribe();
+});
